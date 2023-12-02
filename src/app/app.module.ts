82999baf35db945eba41307799799c8fd3ba08a5
Andres F Tenjo <useda.theli@gmail.com>
2023-12-02 02:44:26 +0000

    adding first landing page

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HeroComponent,
+    LandingpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
